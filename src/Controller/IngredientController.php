f7d319b9c80810be2a5a55cdd6b556d5510fcfb5
Jud <r.judicael@gmail.com>
2023-03-16 22:11:38 +0100

    create form + receipe & ingredient controller

---
+++ src/Controller/IngredientController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ingredient;
+use App\Form\IngredientType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IngredientController extends AbstractController
+{
+    #[Route('/ingredient', name: 'app_ingredient')]
+    public function index(Request $request, EntityManagerInterface $em): Response
+    {
+        
+        $ingredient = new Ingredient;
+        
+        $form = $this->createForm(IngredientType::class, $ingredient);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            dd($form->get('ingredientName'));
+            // ... perform some action, such as saving the task to the database
+
+        }
+
+        return $this->render('ingredient/ingredient.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
